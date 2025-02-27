f61a3bfdb2551931e26acdfa1b1b371fbd66b648
biblecampvictim6969 <stefko.richard@norskakladno.cz>
2025-02-27 16:35:56 +0100

    pablo is fricky

---
+++ what_food_would_u_like_to_eat/program.py
@@ -0,0 +1,22 @@
+d={
+    "a":"tvuj bugir vybouchnul", 
+      "b":"pizza se stala pablem a znásilnil tě ", 
+      "c": "taco ti dalo uplnou sračku a umřel si"
+      
+      
+     
+ }
+
+
+print("co chceš jíst") 
+print("a)bugir") 
+print("b)pizza") 
+print("c) taco") 
+
+jidlo=input()
+
+print(d.get(jidlo, "tvoje koule byly odebrány"))
+
+
+
+
