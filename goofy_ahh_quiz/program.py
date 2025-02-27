41c7c134708219237f20a7a40257b80e69a602f3
biblecampvictim6969 <stefko.richard@norskakladno.cz>
2025-02-27 16:36:36 +0100

    i like chesse

---
+++ goofy_ahh_quiz/program.py
@@ -0,0 +1,54 @@
+body=0
+
+
+print("what is obamas last name")
+print ("a:nobody knows")
+print ("b:i like chesse")
+print ("c:uhhhhh lemon")
+       
+odpoved=input()
+if odpoved=="a":
+    body+=1
+    print ("ur smartASS")   
+
+else:    
+    print("nobody will never love u")  
+
+
+
+
+print ("who was  P diddy:")
+print ("a: a great uncle")
+print ("b: A RAPIST")
+print ("c: a austrian painter")
+
+odpoved=input()
+if odpoved=="b":
+    body+=1
+    print ("ur smartASS")   
+
+else:    
+    print("nobody will never love u")  
+
+
+
+
+print ("What will u do if u see a baby crying:")
+print ("a: launch a nuclear strike to quiet him")
+print ("b: pull out a 19 calliber sniper and shoot him")
+print ("c: do bad things to him")
+
+odpoved= input()
+if odpoved=="b":
+    body+=1
+    print ("ur smartASS")   
+
+else:    
+    print("nobody will never love u")  
+
+ 
+
+
+
+print("u got", body )
+print("hope ur proud of ur self")
